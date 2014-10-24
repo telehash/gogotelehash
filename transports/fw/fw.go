1070159045d141b6bb8bc0132aa9946d36a39c4e
Simon Menke <simon.menke@gmail.com>
2014-10-24 14:15:42 +0200

    Added a basic firewall transport (mainly for testing purposes)

---
+++ transports/fw/fw.go
@@ -0,0 +1,62 @@
+package fw
+
+import (
+	"bitbucket.org/simonmenke/go-telehash/transports"
+)
+
+var (
+	_ transports.Config    = Config{}
+	_ transports.Transport = (*firewall)(nil)
+)
+
+type Config struct {
+	Config transports.Config
+	Allow  Rule
+}
+
+type Rule interface {
+	Match(p []byte, src transports.Addr) bool
+}
+
+type firewall struct {
+	t    transports.Transport
+	rule Rule
+}
+
+func (c Config) Open() (transports.Transport, error) {
+	t, err := c.Config.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	return &firewall{t, c.Allow}, nil
+}
+
+func (fw *firewall) LocalAddresses() []transports.Addr {
+	return fw.t.LocalAddresses()
+}
+
+func (fw *firewall) ReadMessage(p []byte) (n int, src transports.Addr, err error) {
+	for {
+		n, src, err = fw.t.ReadMessage(p)
+		if err != nil {
+			return n, src, err
+		}
+
+		if fw.rule == nil || fw.rule.Match(p[:n], src) {
+			return n, src, err
+		}
+
+		// continue
+	}
+
+	panic("unreachable")
+}
+
+func (fw *firewall) WriteMessage(p []byte, dst transports.Addr) error {
+	return fw.t.WriteMessage(p, dst)
+}
+
+func (fw *firewall) Close() error {
+	return fw.t.Close()
+}
---
+++ transports/fw/rules.go
@@ -0,0 +1,91 @@
+package fw
+
+import (
+	"bitbucket.org/simonmenke/go-telehash/transports"
+)
+
+var (
+	_ Rule = RuleFunc(nil)
+	_ Rule = (*negateRule)(nil)
+)
+
+type RuleFunc func(p []byte, src transports.Addr) bool
+
+func (r RuleFunc) Match(p []byte, src transports.Addr) bool {
+	return r(p, src)
+}
+
+var (
+	None Rule = matchNoneRule("deny all")
+	All  Rule = matchAllRule("allow all")
+)
+
+type (
+	matchNoneRule string
+	matchAllRule  string
+)
+
+func (r matchNoneRule) Match(p []byte, src transports.Addr) bool { return false }
+func (r matchAllRule) Match(p []byte, src transports.Addr) bool  { return true }
+
+// Negate matches when r doesn't Match
+func Negate(r Rule) Rule {
+	if r == nil {
+		return None
+	}
+	return &negateRule{r}
+}
+
+type negateRule struct{ Rule }
+
+func (r *negateRule) Allow(p []byte, src transports.Addr) bool { return !r.Rule.Match(p, src) }
+
+// WhenAll matches when all rules Match
+func WhenAll(rules ...Rule) Rule {
+	if len(rules) == 0 {
+		return None
+	}
+
+	if len(rules) == 1 {
+		return rules[0]
+	}
+
+	return RuleFunc(func(p []byte, src transports.Addr) bool {
+		for _, rule := range rules {
+			if !rule.Match(p, src) {
+				return false
+			}
+		}
+		return true
+	})
+}
+
+// WhenNone denys a packet when all the rules Allow it
+func WhenNone(rules ...Rule) Rule {
+	return Negate(WhenAll(rules...))
+}
+
+func WhenAny(rules ...Rule) Rule {
+	if len(rules) == 0 {
+		return None
+	}
+
+	if len(rules) == 1 {
+		return rules[0]
+	}
+
+	return RuleFunc(func(p []byte, src transports.Addr) bool {
+		for _, rule := range rules {
+			if rule.Match(p, src) {
+				return true
+			}
+		}
+		return false
+	})
+}
+
+func From(addr transports.Addr) Rule {
+	return RuleFunc(func(p []byte, src transports.Addr) bool {
+		return transports.EqualAddr(src, addr)
+	})
+}
